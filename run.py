9dee767ad7f97f50471e430ce35e87762ffe17d3
Mohammad Haseeb <mohammad.haseeb@xivtech.io>
2022-06-05 15:29:53 +0530

    Second commit

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import random
+import sys 
+
 from Bat_Algorithm_tsp import *
-n = input("Enter your number of bats: ")
-t = input("Enter your number of iterations ")
-MX = sys.maxint
+n = int(input("Enter your number of bats: "))
+t = int(input("Enter your number of iterations "))
+MX = 100000
 xbest = [1,2,3,4]
 cities = [1, 2, 3, 4]
 graph = [[0, 10, 15, 20], [10, 0, 35, 25],
@@ -10,10 +12,10 @@ graph = [[0, 10, 15, 20], [10, 0, 35, 25],
 sol = []
 R = []
 A = []
-for i in range(n):
-    sol[i] = random.shuffle(cities)
-    R[i] = round(random.uniform(0.4, 0.5), 2)
-    A[i] = round(random.uniform(0.4, 0.5), 2)
+for i in range(0,n):
+    sol.append(random.shuffle(cities))
+    R.append(round(random.uniform(0.4, 0.5), 2))
+    A.append(round(random.uniform(0.4, 0.5), 2))
 for i in range(t):
     c = []
     for i in range(n):
@@ -21,17 +23,21 @@ for i in range(t):
         temp = bat_obj.solve()
         c[i] = temp[0]
         sol[i] = temp[1]
-    cbest = sys.maxint
+    cbest = 100000
     xcbest = []
+    tr=-1
     r = round(random.uniform(0.0, 1.0), 2)
     for i in range (n):
         if(r > R[i]):
             if(c<cbest):
                 cbest = c[i]
                 xcbest = sol[i] 
-    TT = calculate_cost(xcbest,graph)
-    if(TT<MX):
-        MX = TT
+                tr=i
+    #TT = calculate_cost(xcbest,graph)
+    if(cbest<MX):
+        MX = cbest
         xbest = xcbest
+        R[tr] = min(1,R[tr]+0.15)
+        A[tr] = max(0,A[tr]-0.15)
 print("The best sequence is ",xbest)
 print ("Its cost is ",MX)
